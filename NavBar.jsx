56b5df89e4f4b71edaf0fcf4cf4bc714fb676c61
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-07-20 22:27:47 +0530

    Update NavBar.jsx

--- NavBar.jsx
+++ NavBar.jsx
@@ -1,16 +1,1 @@
-import { Link } from 'react-router-dom';
 
-export default function Navbar() {
-  return (
-    <nav className="flex justify-between items-center p-4 bg-black shadow-md">
-      <img src="/logo.jpg" alt="MetaMindset Labs" className="h-10" />
-      <ul className="flex space-x-6 text-sm font-semibold">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/articles">Articles</Link></li>
-        <li><Link to="/join">Join</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
-  );
-}
